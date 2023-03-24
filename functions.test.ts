6c09ae46611f7c3d7d1d55956d55053b6e0c3934
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-24 15:59:02 -0600

    struggggling through tests

--- functions.test.ts
+++ functions.test.ts
@@ -2,6 +2,6 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     test('return an array', () => {
-        expect(typeof shuffleArray()).toHaveReturned(array)
+        expect(shuffleArray()).toReturn()
     })
 })
