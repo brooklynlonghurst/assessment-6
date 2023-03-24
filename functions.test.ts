441ac8919c25a764f6253232cdc75770e7606ce1
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-24 13:24:23 -0600

    inital commit

---
+++ functions.test.ts
@@ -0,0 +1,7 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    test('return an array', () => {
+        expect(typeof shuffleArray()).toHaveReturned(array)
+    })
+})
