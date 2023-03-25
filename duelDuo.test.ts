9a518c493880cc22c9531fe2252b758184bc27b3
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-24 19:02:29 -0600

    completed and got tests passing

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    driver.get('http://localhost:4000/')
 })
 
 afterAll(async () => {
@@ -19,6 +19,17 @@ test('Title shows up when page loads', async () => {
     expect(displayed).toBe(true)
 })
 
-// test('Check the draw button ', async () => {
+test('Checks the draw button is functional ', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(1000)
+})
 
-// })
+test('Checks to if div is sisplayed after clicking draw button', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(1000)
+    
+    const drawBtn = await driver.findElement(By.id('choices'))
+    const isDisplay = await drawBtn.isDisplayed()
+    await driver.sleep(1000)
+    expect(isDisplay).toBe(true)
+})
--- functions.test.ts
+++ functions.test.ts
@@ -2,6 +2,12 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     test('return an array', () => {
-        expect(shuffleArray()).toReturn()
+        let shuffledArr = shuffleArray('array')
+        expect(shuffledArr).toBeInstanceOf(Array)
+    })
+
+    test('checks that it returns an array of the same length as the argument sent in', () => {
+        let shuffledArr = shuffleArray('array')
+        expect(shuffledArr).not.toHaveLength(0)
     })
 })
